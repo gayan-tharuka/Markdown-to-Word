79bb552bc0d2498784230a98b0388e24dc79cbf2
R.M.K.G.T.Rathnayaka <140826238+gayan-tharuka@users.noreply.github.com>
2025-04-15 17:47:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/scribe-doc-studio/",
   server: {
     host: "::",
     port: 8080,
